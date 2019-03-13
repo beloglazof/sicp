28acc76152223664a9341c795c89c87886ad609b
Slava Beloglazov <supsupno@gmail.com>
2019-03-13 11:26:50 +0500

    add some tests

--- exercises/chapter-1/ex1.3.rkt
+++ exercises/chapter-1/ex1.3.rkt
@@ -1,4 +1,7 @@
-#lang racket
+#lang racket/base
+
+(require rackunit)
+
 (define (square x)(* x x))
 (define (sum-of-squares num1 num2) (+ (square num1) (square num2)))
 (define (sum-of-squares-of-greaters a b c)
@@ -7,3 +10,7 @@
                (else (sum-of-squares a c))))
         (else (cond ((> a c) (sum-of-squares a b))
                     (else (sum-of-squares b c))))))
+
+(check-equal? (sum-of-squares-of-greaters 0 0 0) 0)
+(check-equal? (sum-of-squares-of-greaters 5 4 3) (sum-of-squares 5 4))
+(check-equal? (sum-of-squares-of-greaters -32 -324 3) (sum-of-squares 3 -32))
